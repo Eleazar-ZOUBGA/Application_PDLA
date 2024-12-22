ace9a43d22ba78d54e804a7052a41a256b10b009
Matthias FORNARO <matthias.fornaro@gmail.com>
2024-12-22 22:13:57 +0100

    test modifiaction des mission dans la BDD

--- src/test/java/DataSourceTest.java
+++ src/test/java/DataSourceTest.java
@@ -62,6 +62,22 @@ public class DataSourceTest {
         }
     }
 
+    
+    @Test
+    public void testGetMissionByStatusNonValidee() {
+        if (access) {
+            DataSource.startConnection();
+            User user = new VulnerableUser(1, "d", "d", "d@d", "d");
+            Mission mission = new Mission("Transport", "Transporter un colis", new Date(), "Marseille");
+            mission.setStatutMission("Non Validée");
+            DataSource.registerMission(mission, user);
+            ArrayList<Mission> missions = DataSource.getMissionsByStatus("Non Validée");
+            for (Mission mission1 : missions){
+                assertEquals("Non Validée", mission1.getStatutMission());
+            }    
+        }
+    }
+
     @Test
     public void testGetMissionByStatusRealisee() {
         if (access) {
@@ -77,5 +93,44 @@ public class DataSourceTest {
         }
     }
 
+    @Test
+    public void testupdateStatusMission() {
+        if (access) {
+            DataSource.startConnection();
+            User user = new VulnerableUser(1, "d", "d", "d@d", "d");
+            Mission mission = new Mission("Lavage", "Laver des vêtements", new Date(), "Toulouse");
+            DataSource.registerMission(mission, user);
+            ArrayList<Mission> missions = DataSource.getMissionsByStatus("En Attente");
+            mission = missions.getLast();
+            mission.setStatutMission("Validée");
+            DataSource.updateStatusMission(mission);
+            assertEquals(mission, missions.getLast());
+            mission.setStatutMission("Non Validée");
+            DataSource.updateStatusMission(mission);
+            assertEquals(mission, missions.getLast());
+            mission.setStatutMission("Réalisée");
+            DataSource.updateStatusMission(mission);
+            assertEquals(mission, missions.getLast());
+        }
+    }
 
+    @Test
+    public void testsetMotifMission() {
+        if (access) {
+            DataSource.startConnection();
+            User user = new VulnerableUser(1, "d", "d", "d@d", "d");
+            Mission mission = new Mission("Lavage", "Laver des vêtements", new Date(), "Toulouse");
+            mission.setStatutMission("Non Validée");
+            DataSource.registerMission(mission, user);
+            ArrayList<Mission> missions = DataSource.getMissionsByStatus("Non Validée");
+            mission = missions.getLast();
+            String motif = "Test";
+            mission.setMotifMissionNonValidee(motif);
+            DataSource.setMotifMission(mission);
+            missions = DataSource.getMissionsByStatus("Non Validée");
+            mission = missions.getLast();
+            assertEquals(motif, mission.getMotifMissionNonValidee());
+              
+        }
+    }
 }
